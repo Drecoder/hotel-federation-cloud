3e0a28d1295ae5b9a193efffbace1400769ad5b4
Andres Arias <ariasa83@gmail.com>
2026-04-15 15:37:08 -0500

    run CI/CD

--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -14,7 +14,7 @@ variable "project_name" {
 
 # 3. Networking Defaults
 variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
+  description = "The CIDR block for the VPC."
   type        = string
   default     = "10.0.0.0/16"
 }
