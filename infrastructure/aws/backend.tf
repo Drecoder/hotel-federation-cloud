6a9c654c5a1bcd0c196bb0b272885f4ab047d439
Andres Arias <ariasa83@gmail.com>
2026-04-15 15:26:56 -0500

    run CI/CD

--- infrastructure/aws/backend.tf
+++ infrastructure/aws/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "hotel-federation/vpc/terraform.tfstate"
     region         = "us-east-1"
     
-    # This enables state locking via DynamoDB
+    # This enables state locking via DynamoDB.
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
